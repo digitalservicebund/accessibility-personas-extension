7debe042520efbdd9456192296cb90fb7dcc9af8
josh-nowak <joshuanowak.jn@gmail.com>
2024-05-11 01:55:38 +0200

    persisting changes across pages, and showing state in the extension popup

--- background.js
+++ background.js
@@ -1,31 +1,70 @@
-// Listen for messages from other parts of the extension
+// Listen for requests to update the persona
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "updatePersona") {
     // Get the CSS and JS file names from the message
     const cssFile = request.cssFile;
     const jsFile = request.jsFile || null;
-    //const tabId = sender.tab.id;
+    const personaName = request.personaName;
+    const tabId = request.tabId;
 
     // Insert the CSS and JS files into the current tab
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.scripting.removeCSS(
-        {
-          target: { tabId: tabs[0].id },
-          files: ["ashleigh.css", "chris.css", "pawel.css", "ron.css"],
-        },
-        () => {
-          chrome.scripting.insertCSS({
-            target: { tabId: tabs[0].id },
-            files: [cssFile],
-          });
-        }
-      );
+      // Store the relevant files in local storage
+      chrome.storage.local.set({ [tabId]: { cssFile, jsFile, personaName } });
+
+      // Remove any existing CSS files
+      if (cssFile) {
+        chrome.scripting.removeCSS(
+          {
+            target: { tabId },
+            files: ["ashleigh.css", "chris.css", "pawel.css", "ron.css"],
+          },
+          // Insert the new CSS file
+          () => {
+            chrome.scripting.insertCSS({
+              target: { tabId },
+              files: [cssFile],
+            });
+          }
+        );
+      }
 
       if (jsFile) {
         chrome.scripting.executeScript({
-          target: { tabId: tabs[0].id },
+          target: { tabId },
           files: [jsFile],
         });
+      }
+    });
+  }
+});
+
+// Listen for tab updates, and re-insert the CSS and JS files if necessary
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === "loading") {
+    chrome.storage.local.get([tabId.toString()], function (result) {
+      console.log("result", result);
+      if (result[tabId]) {
+        const { cssFile, jsFile } = result[tabId];
+        // Reinsert CSS
+        if (cssFile) {
+          chrome.scripting
+            .insertCSS({
+              target: { tabId },
+              files: [cssFile],
+            })
+            .catch((error) => console.error("Failed to insert CSS:", error));
+        }
+
+        // Re-execute JS
+        if (jsFile) {
+          chrome.scripting
+            .executeScript({
+              target: { tabId },
+              files: [jsFile],
+            })
+            .catch((error) => console.error("Failed to execute JS:", error));
+        }
       }
     });
   }
--- popup.js
+++ popup.js
@@ -1,22 +1,88 @@
+function formatPersonaList() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const currentTabId = tabs[0].id;
+
+    chrome.storage.local.get([currentTabId.toString()], function (result) {
+      if (result[currentTabId]) {
+        // Extract the personaName
+        const personaName = result[currentTabId].personaName;
+
+        if (personaName !== undefined) {
+          // Disable all buttons
+          document.querySelectorAll(".select-persona").forEach((button) => {
+            button.disabled = true;
+          });
+
+          // Hide the selected button
+          document.getElementById(personaName).style.display = "none";
+          // Display the respective instruction button instead
+          const selector = `.instructions[data-overlay="${personaName}Overlay"]`;
+          console.log("selector", selector);
+          const personaInstructionButton = document.querySelector(selector);
+          console.log("personaInstructionButton", personaInstructionButton);
+          personaInstructionButton.style.display = "block";
+
+          // Format the selected persona
+          personaInstructionButton.parentElement.parentElement.classList.add(
+            "selected-persona"
+          );
+
+          // Show the hint to open a new tab for a new persona
+          document.getElementById("new-tab-hint").style.display = "block";
+        }
+      }
+    });
+  });
+}
+
+formatPersonaList();
+
+// Listen to clicks on the select-persona buttons
 document.querySelectorAll(".select-persona").forEach((button) => {
   button.addEventListener("click", function () {
     // Get the CSS and JS file names from the data attributes
     const cssFile = this.getAttribute("data-css");
     const jsFile = this.getAttribute("data-js") || null;
+    const personaName = this.getAttribute("id");
 
-    // Send a message to the background script to update the persona
-    chrome.runtime.sendMessage({ action: "updatePersona", cssFile, jsFile });
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const currentTabId = tabs[0].id;
+
+      // Send a message to the background script to update the persona and enable simulation
+      chrome.runtime.sendMessage({
+        action: "updatePersona",
+        cssFile,
+        jsFile,
+        personaName,
+        tabId: currentTabId,
+      });
+    });
 
     // Show the overlay with persona details and instructions
     const overlayId = this.getAttribute("data-overlay");
     document.getElementById(overlayId).style.display = "block";
-    console.log("overlayId", overlayId);
+
+    // Format the persona list
+    formatPersonaList();
   });
 });
 
-document.querySelectorAll(".close-overlay").forEach((button) => {
-  // Hide the overlay when the close button is clicked
+// Listen to clicks on the instructions buttons, and show the overlay
+document.querySelectorAll(".instructions").forEach((button) => {
   button.addEventListener("click", function () {
+    // Show the overlay with persona details and instructions
+    const overlayId = this.getAttribute("data-overlay");
+    document.getElementById(overlayId).style.display = "block";
+  });
+});
+
+// Listen to clicks on the close-overlay button
+document.querySelectorAll(".close-overlay").forEach((button) => {
+  button.addEventListener("click", function () {
+    // Hide the overlay
     this.parentElement.parentElement.style.display = "none";
+
+    // Format the persona list
+    formatPersonaList();
   });
 });

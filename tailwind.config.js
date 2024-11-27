75a9ec4c4550011b9e4174743d05c0fdbbdf7c42
josh-nowak <joshuanowak.jn@gmail.com>
2024-11-27 16:42:05 +0100

    improve link and button focus states for accessibility

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,26 @@
 module.exports = {
   content: ["./**/*.{html,js,json}"],
   theme: {
-    extend: {},
+    extend: {
+      ringWidth: {
+        DEFAULT: "2px",
+      },
+      ringColor: {
+        focus: "#000",
+      },
+      colors: {
+        "focus-bg": "#fec32bff",
+      },
+    },
     fontFamily: {
       sans: ["Roboto", "sans-serif"],
+    },
+  },
+  variants: {
+    extend: {
+      ringWidth: ["focus"],
+      ringColor: ["focus"],
+      backgroundColor: ["focus"],
     },
   },
   plugins: [],
